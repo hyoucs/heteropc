d58e61c3419ad9f9dec6beb094239896eb9dd74c
Hongyuan <hyoucs>
2018-03-07 21:09:16 -0800

    proximate mask

--- model_analysis/t_edge_adjacency_mask.m
+++ model_analysis/t_edge_adjacency_mask.m
@@ -1,7 +1,7 @@
 function M = t_edge_adjacency_mask(n)
 
 	n_edge = (n*(n-1)/2);
-	A = inline_vec2mat(1:n_edge, n);
+	A = t_vec2mat(1:n_edge, n);
 	M = eye(n_edge, n_edge);
 	for i = 1:n,
 		edge_idx = A(i,:);
